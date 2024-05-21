0d7f8b823bf783d250e7d4fb978ecc9ab43fbeda
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-21 19:19:40 -0300

    feat: blueprint route

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
 
 
 #Inicialização
 app = Flask(__name__)
 
-@app.route('/')
-def ola_mundo():
-    titulo = "Gestão de Usuarios"
+app.register_blueprint(home_route)
 
-    usuarios = [
-        {"nome": "Guilherme", "membro_ativo": True},
-        {"nome": "João", "membro_ativo": False},
-        {"nome": "Maria", "membro_ativo": False}
-    ]
-
-    return render_template('index.html', titulo=titulo, usuarios=usuarios)
-
-
-@app.route('/sobre')
-def pagina_sobre():
-    return """
-        <b>Programador Python </b>
-        """
 
 app.run(debug=True)
