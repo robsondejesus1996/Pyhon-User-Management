9ab16c0ec9d6f90e81ff3393a7168269d687dfc2
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-22 21:10:16 -0300

    feat: create post, delete route front

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database.cliente import CLIENTES
 
 
@@ -12,7 +12,20 @@ def lista_clientes():
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
     """ Inserir os dados do cliente """
-    pass
+
+    data = request.json
+
+    novo_usuario = {
+        "id": len(CLIENTES) + 1,
+        "nome": data['nome'],
+        "email": data['email'],
+    }
+
+    CLIENTES.append(novo_usuario)
+
+
+    return render_template('item_cliente.html', cliente=novo_usuario)
+    
     
 @cliente_route.route('/new')
 def form_cliente():
@@ -35,6 +48,8 @@ def atualizar_cliente (cliente_id):
     pass  
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
-def delete_cliente (cliente_id):
+def delete_cliente(cliente_id):
     """ Deletar um cliente """
-    pass  
+    global CLIENTES
+    CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id ]  
+    return {'deleted': 'ok'}
