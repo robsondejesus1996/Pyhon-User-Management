8e82843ffe7cddd0f3ab1b5f43af4154a115c046
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-23 21:40:57 -0300

    feat: create update, delete, see

--- routes/cliente.py
+++ routes/cliente.py
@@ -35,17 +35,35 @@ def form_cliente():
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     """ Exibir detalhes do cliente """
-    return render_template('detalhe_cliente.html')  
+    cliente = list(filter(lambda c: c['id'] == cliente_id, CLIENTES)) [0]
+    return render_template('detalhe_cliente.html', cliente=cliente)  
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente (cliente_id):
     """ Formulario para editar um cliente """
-    return render_template('form_edit_cliente.html')  
+    cliente = None
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            cliente = c
+
+    return render_template('form_cliente.html', cliente=cliente)  
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente (cliente_id):
     """ Atualizar informações do cliente """
-    pass  
+    cliente_editado = None
+
+    data = request.json
+
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']     
+            c['email'] = data['email']
+
+            cliente_editado = c
+
+    return render_template('item_cliente.html', cliente=cliente_editado)        
+      
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def delete_cliente(cliente_id):
