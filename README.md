e94ee05dbfb38a5f2b1a84b0a20096ff698173b6
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-24 20:40:28 -0300

    feat: project completion, create database and converte mock for Sdlitedatabase

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Pyhon-User-Management
+<p align="center">
+  <a href="#">
+    <img src="https://github.com/robsondejesus1996/Pyhon-User-Management/assets/31260719/bf2dc700-b5ed-45aa-8d20-22cb391144b0" alt="Python: Full Stack APP">
+  </a>
+</p>
+
+<hr/>
+
+<p align="center">
+  <a href="#">
+    <img src="https://github.com/robsondejesus1996/Pyhon-User-Management/assets/31260719/ad0d2210-61af-40af-aa5f-65b01cc58744" alt="Python: Full Stack APP Banco">
+  </a>
+</p>
+
+
+# Customer Management with Flask, Bootstrap, and JavaScript
+
+## Overview
+This project is a web application for customer management developed using the Flask API in Python. The main objective is to allow users to manage a customer database, offering functionalities to register, edit, delete, and view customer details in a modal. The user interface was created using HTML, Bootstrap for styling, and JavaScript for dynamic page manipulation.
+
+## Technologies Used
+- **Backend**: Flask (Python)
+- **Frontend**: HTML, Bootstrap, JavaScript
+- **Database**: SQLite (or any other database compatible with Flask SQLAlchemy)
+
+## Features
+
+### Customer Registration
+- Form to add a new customer with fields for name and email.
+- Server-side data validation to ensure that the name and email are provided and that the email is unique.
+
+### Customer Listing
+- Display all registered customers in a table.
+- Action buttons in the table to edit, delete, or view detailed information about the customers.
+
+### Customer Editing
+- Form to edit an existing customer's information.
+- Auto-fill the form with the current data of the customer to facilitate editing.
+
+### Customer Deletion
+- Option to delete a customer from the database.
+- Confirmation before deletion to prevent accidental removals.
+
+### Viewing Customer Details
+- Modal to display detailed information about the customer.
+- Button in the customer listing table to open the modal with the selected customer's information.
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from database.cliente import CLIENTES
+from database.models.cliente import Cliente
 
 
 cliente_route = Blueprint('cliente', __name__)
@@ -7,7 +7,8 @@ cliente_route = Blueprint('cliente', __name__)
 @cliente_route.route('/')
 def lista_clientes():
     """Listar os Clientes"""
-    return render_template('lista_clientes.html',clientes=CLIENTES)
+    clientes = Cliente.select()
+    return render_template('lista_clientes.html',clientes=clientes)
 
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
@@ -15,13 +16,10 @@ def inserir_cliente():
 
     data = request.json
 
-    novo_usuario = {
-        "id": len(CLIENTES) + 1,
-        "nome": data['nome'],
-        "email": data['email'],
-    }
-
-    CLIENTES.append(novo_usuario)
+    novo_usuario = Cliente.create(
+        nome = data['nome'],
+        email = data['email'],
+    )
 
 
     return render_template('item_cliente.html', cliente=novo_usuario)
@@ -35,32 +33,27 @@ def form_cliente():
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     """ Exibir detalhes do cliente """
-    cliente = list(filter(lambda c: c['id'] == cliente_id, CLIENTES)) [0]
+
+    cliente = Cliente.get_by_id(cliente_id)
     return render_template('detalhe_cliente.html', cliente=cliente)  
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente (cliente_id):
     """ Formulario para editar um cliente """
-    cliente = None
-    for c in CLIENTES:
-        if c['id'] == cliente_id:
-            cliente = c
+    cliente = Cliente.get_by_id(cliente_id)
 
     return render_template('form_cliente.html', cliente=cliente)  
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente (cliente_id):
     """ Atualizar informações do cliente """
-    cliente_editado = None
-
+    
     data = request.json
 
-    for c in CLIENTES:
-        if c['id'] == cliente_id:
-            c['nome'] = data['nome']     
-            c['email'] = data['email']
-
-            cliente_editado = c
+    cliente_editado = Cliente.get_by_id(cliente_id)
+    cliente_editado.nome = data['nome']
+    cliente_editado.email = data['email']
+    cliente_editado.save()
 
     return render_template('item_cliente.html', cliente=cliente_editado)        
       
@@ -68,6 +61,6 @@ def atualizar_cliente (cliente_id):
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def delete_cliente(cliente_id):
     """ Deletar um cliente """
-    global CLIENTES
-    CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id ]  
+    cliente = Cliente.get_by_id(cliente_id)
+    cliente.delete_instance()
     return {'deleted': 'ok'}
